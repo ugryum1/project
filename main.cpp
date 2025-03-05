384ae2470090841a72b70239c5b136ac0cf959ac
ugryum1 <n.a.ugryumov@gmail.com>
2025-03-05 19:47:01 +0300

    created a simple application

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
-#include <iostream>
+#include <QApplication>
+#include <QPushButton>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
 
+    QPushButton button("Hello, Qt!");
+    button.resize(200, 100);
+    button.show();
+
+    return app.exec();
 }
